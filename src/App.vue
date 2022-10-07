16378bc47a6a141da56ad79b13a715efe0e8705e
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-07 21:41:47 +0330

    Handled state and method for change state of show sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      showSidebar: false
+    }
+  },
+  methods: {
+    setShow() {
+      this.showSidebar = !this.showSidebar;
+    }
+  }
+}
 </script>
 
 <template>
   <div class="items_center">
-    <button class="button button_icon">
+    <button @click="setShow" class="button_icon" :class="showSidebar && 'active'">
       <span></span>
       <span></span>
     </button>
@@ -21,23 +32,30 @@
   .button_icon {
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    height: 100px;
+    border: none;
+    outline: none;
+    background: transparent;
+    cursor: pointer;
   }
   .button_icon span {
     display: block;
     background-color: black;
-    width: 150px;
+    width: 100px;
     height: 3px;
   }
   .button_icon span:first-child {
-    transform: translateY(-50px);
+    transform: translateY(-25px);
   }
-  .button_icon span:first-child {
-    transform: translateY(50px);
+  .button_icon span:last-child {
+    transform: translateY(25px);
   }
   .button_icon.active span:first-child {
     transform: rotate(-45deg)
   }
-  .button_icon.active span:first-child {
+  .button_icon.active span:last-child {
     transform: rotate(45deg)
   }
 </style>
